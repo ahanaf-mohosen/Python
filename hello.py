e2a90c977a11c3f160016cb439f3172e4dada8ef
MD. AHANAF MOHOSEN <ahanaf537@gmail.com>
2024-07-11 00:28:10 +0600

    Initial commit

---
+++ hello.py
@@ -0,0 +1,34 @@
+
+tup = ("Mugdho",5,5.7)
+print(type(tup))
+
+
+
+
+
+
+
+
+
+
+
+"""print(input("name: "))"""
+'''print("What is your name?")
+name = input("=> ")
+print("Hello ", name)
+
+print("What is your age?")
+age = int(input("=> "))
+'''
+'''if age >= 18:
+    print("You are elegible for voting")
+else:
+    print("you are not elegible for voting")
+'''
+
+'''vote = "elegible" if age >= 18 else "not elegible"
+print(vote)
+'''
+
+'''vote = ("no","yes")[age>=18]
+print(vote)'''
